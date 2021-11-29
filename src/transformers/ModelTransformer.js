ac882eaf57a5813f4a56f0eeaabbb0ac1f7c430d
Jonas Müller <ref@jonmaxmue.de>
2021-11-29 16:57:33 +0100

    detection of failor

--- src/transformers/ModelTransformer.js
+++ src/transformers/ModelTransformer.js
@@ -76,7 +76,7 @@ export default class {
       });
     }
 
-    if (data.relationships && data.attributes) {
+    if (data.relationships) {
       Object.entries(data.relationships).forEach(([relationName, data]) => {
         // Convert JSON:API casing to Vuex ORM casing and look up the relation.
         relationName = this.resourceToEntityCase(relationName);
